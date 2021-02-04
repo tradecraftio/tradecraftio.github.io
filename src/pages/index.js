df3a802b85f8bcb4b61377cd24c433a14b3bf04f
Mark Friedenbach <mark@friedenbach.org>
2021-02-04 13:58:51 -0800

    Remove the "using typescript" page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
